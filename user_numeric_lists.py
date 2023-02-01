f96d5ae8687d731ba91bb17b9d7560424c3d267f
lanarokip <lankipkoecharo@gmail.com>
2023-02-02 00:16:14 +0300

    created list1 listx and listy and calculated the mean stdand median

--- user_numeric_lists.py
+++ user_numeric_lists.py
@@ -6,6 +6,7 @@ Modify this docstring.
 # import some modules first - how many can you make use of?
 
 import math
+import statistics as stats
 
 
 # define some functions
@@ -44,21 +45,27 @@ if __name__ == "__main__":
     # call your functions here (see instructions)
     print("your code here")
 
+list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+listx = list(range(10))
+listy = [1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5, 9.5, 10.5]
 
-# Why? Why only print if this the module called?
-# Because when you write good functions, you may want to
-# import this module into another script - just like you did
-# math or statistics.
-# Build a library of resuable functions to support your domain.
+# Mean
+mean = stats.mean(list1)
+print("Mean:", mean)
 
-# For example, if your domain:
-# Is sports, create functions to provide a list of teams.
-# Is pets, create functions to calculate adoption prices.
-# Is music, create functions to return a list of your favorite artists.
+# Median
+median = stats.median(list1)
+print("Median:", median)
 
+# Mode
+mode = stats.mode(list1)
+print("Mode:", mode)
 
-# When you write reusable functions for your domain, you can
-# import the module with your utility functions into other modules
-# and use them there.  This is a very common practice.
-# Anything you write can be imported into later projects.
+# Standard Deviation
+stdev = stats.stdev(list1)
+print("Standard Deviation:", stdev)
+
+# Variance
+variance = stats.variance(list1)
+print("Variance:", variance)
 
